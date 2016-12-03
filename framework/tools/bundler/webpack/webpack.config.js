3ed5160f6c04ab0be1de8bc52aaf0242c8219c97
Marek Piasecki <mad@maniak.pro>
2016-12-03 12:00:34 +0100

    add ability to load fonts

--- framework/tools/bundler/webpack/webpack.config.js
+++ framework/tools/bundler/webpack/webpack.config.js
@@ -28,6 +28,11 @@ module.exports = {
             'img'
         ]
       },
+      { test: /\.woff$/, loader: 'url?limit=65000&mimetype=application/font-woff&name=public/fonts/[name].[ext]' },
+      { test: /\.woff2$/, loader: 'url?limit=65000&mimetype=application/font-woff2&name=public/fonts/[name].[ext]' },
+      { test: /\.[ot]tf$/, loader: 'url?limit=65000&mimetype=application/octet-stream&name=public/fonts/[name].[ext]' },
+      { test: /\.eot$/, loader: 'url?limit=65000&mimetype=application/vnd.ms-fontobject&name=public/fonts/[name].[ext]' },
+
   	  { test: /\.imba/, loader: 'imba/loader' },
   	  { test: /\.coffee/, loader: 'coffee-loader' }
     ]
