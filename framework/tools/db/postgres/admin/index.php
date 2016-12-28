b79e3eff5d628c61ecc3cef9c6584ee269c91d4a
Arkadiusz Fal <arek@arekf.net>
2016-12-28 09:55:08 +0100

    Restore adminer autologin

--- framework/tools/db/postgres/admin/index.php
+++ framework/tools/db/postgres/admin/index.php
@@ -1,4 +1,9 @@
 <?php
+if ($_SERVER["REQUEST_URI"] == '/') {
+  header('Location: http://localhost:8000/?pgsql=' . getenv('POSTGRES_HOST') . '&username=' . getenv('POSTGRES_USER') . '&db=' . getenv('APP'));
+  die();
+}
+
 function adminer_object() {
 
   include_once "./plugins/plugin.php";
