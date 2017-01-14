0476a91972c1fc68b903914a0f0796e24c424f9d
Arkadiusz Fal <arek@arekf.net>
2017-01-14 15:09:09 +0100

    Add docker-compose generator

---
+++ framework/tools/containers/docker/init.rb
@@ -0,0 +1,12 @@
+require 'yaml'
+
+config = YAML.load(File.read('framework/tools/containers/docker/docker-compose.yml'))
+services = Dir['**/*/docker.yml'].inject({}) { |h, c| h.merge(YAML.load(File.read(c))) }
+
+config['services'] = services
+
+puts "Found #{services.count} services:"
+services.keys.each { |s| puts "* #{s}" }
+
+puts "Writing docker-compose.yml..."
+File.write('docker-compose.yml', config.to_yaml)
