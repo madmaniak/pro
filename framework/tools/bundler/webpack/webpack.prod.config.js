13108a7ac9711b3060bb242df6ab2d53262a7d8a
Marek Piasecki <mad@maniak.pro>
2017-08-28 15:50:25 +0200

    smaller bundle size, compression, webpack 3

--- framework/tools/bundler/webpack/webpack.prod.config.js
+++ framework/tools/bundler/webpack/webpack.prod.config.js
@@ -1,5 +1,8 @@
+const webpack = require('webpack');
+const CompressionPlugin = require("compression-webpack-plugin");
 const path = require('path');
 module.exports = {
+  cache: false,
   context: __dirname,
   entry: '../../../services/init',
   node: {
@@ -9,7 +12,35 @@ module.exports = {
 	path: __dirname + '/../../../public',
     filename: 'bundle.js'
   },
-  devtool: 'eval',
+  devtool: 'cheap-module-source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': '"production"'
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      mangle: true,
+      compress: {
+        warnings: false, // Suppress uglification warnings
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        screw_ie8: true
+      },
+      output: {
+        comments: false,
+      },
+      exclude: [/\.min\.js$/gi] // skip pre-minified libs
+    }),
+    new webpack.IgnorePlugin(/^\.\/locale$/, [/moment$/]),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new CompressionPlugin({
+      asset: "[path].gz[query]",
+      algorithm: "gzip",
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0
+    })
+  ],
   module: {
     rules: [
       {
